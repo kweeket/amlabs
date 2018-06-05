1fcae8c33e635fddda15f2bfe5a6fddc0f176fd7
Beck Johnson <rebeckah.j.johnson@avanade.com>
2018-06-05 11:36:27 -0700

    initial files

---
+++ amtest.js
@@ -0,0 +1,9 @@
+﻿$('document').ready(function() {
+
+    $('.hasSubmenu').on('mouseenter', function (e) {
+        e.stopPropagation();
+        $(this).addClass('menuActive').find('ul').slideDown();
+    }).on('mouseleave', function() {
+        $(this).removeClass('menuActive').find('ul').slideUp();
+    });
+});
